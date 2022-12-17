293e65598690ed317c9957b376914c659a0ce156
Emrullah <emr.ozk.71@gmail.com>
2022-12-18 00:48:22 +0300

    Some changes

--- ik-demo/src/NavBar.js
+++ ik-demo/src/NavBar.js
@@ -6,7 +6,7 @@ const NavBar = () => {
 
     return(
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a className="navbar-brand" href="src/NavBar#">Navbar</a>
+        <a className="navbar-brand" href="src/NavBar#">ManageIK</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01"
                 aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
